32ed43f3ca0b886c73332dcf67d1986e3332ad0a
Sameer_Raturi <sameerraturi2001@gmail.com>
2023-09-20 01:27:49 +0530

    observer pattern

---
+++ design-patterns/04.observer-pattern/index.js
@@ -0,0 +1,24 @@
+import {
+  sendToGoogleAnalytics,
+  sendToCustomAnalytics,
+  sendToEmail,
+} from './analytics.js';
+
+import './style.css';
+
+const pinkBtn = document.getElementById('pink-btn');
+const blueBtn = document.getElementById('blue-btn');
+
+pinkBtn.addEventListener('click', () => {
+  const data = '🎀 Click on pink button! 🎀';
+  sendToGoogleAnalytics(data);
+  sendToCustomAnalytics(data);
+  sendToEmail(data);
+});
+
+blueBtn.addEventListener('click', () => {
+  const data = '🦋 Click on blue button! 🦋';
+  sendToGoogleAnalytics(data);
+  sendToCustomAnalytics(data);
+  sendToEmail(data);
+});
